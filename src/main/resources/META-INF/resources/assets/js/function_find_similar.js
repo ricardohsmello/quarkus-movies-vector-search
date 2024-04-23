b8eed05c60f5399645ef9d48d3d9ff8088ee8054
Ricardo <ricardohsmello@gmail.com>
2024-04-23 03:31:50 -0300

    feat: Index

--- src/main/resources/META-INF/resources/assets/js/function_find_similar.js
+++ src/main/resources/META-INF/resources/assets/js/function_find_similar.js
@@ -1,7 +1,7 @@
 async function fetchAndDisplayMovies(data) {
     try {
         const resultsDiv = document.getElementById('movieResults');
-        resultsDiv.innerHTML = '<div class="loading">Searching for movies...</div>';
+        resultsDiv.innerHTML = '<div class="loading"><span class="badge bg-light text-dark" style="font-size: 30px">Loading movies...</span></div>';
 
         const response = await fetch('http://localhost:8080/movies/findSimilar', {
             method: 'POST',
